b192a2cf86befd479ca2cfea96f3cb807d4f25ee
Pablo Bengoa <bengoana@esat-alumni.com>
2020-08-13 16:37:45 +0200

    Added item base classes for inventory

---
+++ Source/ProjectPangea/Public/Core/ConstantItem.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interfaces/ItemLogic.h"
+#include "ConstantItem.generated.h"
+
+UCLASS()
+class PROJECTPANGEA_API AConstantItem : public AActor, public IItemLogic
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AConstantItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+
+  UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Functions")
+    bool Use();
+  //Override this function for item use logic
+  virtual bool Use_Implementation();
+
+};
